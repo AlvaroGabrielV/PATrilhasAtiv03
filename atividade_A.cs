12a2b4e02b267b561936d15847f711f00bf8b46c
AlvaroGabrielV <168542138+AlvaroGabrielV@users.noreply.github.com>
2025-01-29 10:56:11 -0300

    atividade_A.cs

---
+++ atividade_A.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace att02
+{
+	class Program
+	{
+		public static void Main()
+		{	
+			Console.Write("Insira um numero: ");
+			int numero = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Insira um numero: ");
+			int numero2 = Convert.ToInt32(Console.ReadLine());
+
+			if (numero > numero2){
+				Console.WriteLine("\r\nO numero {0} e maior que {1}!", numero, numero2);
+			}
+			else {
+				Console.WriteLine("\r\nO numero {0} não é maior que {1}", numero, numero2);
+			}
+			
+			Console.WriteLine("\r\nAperte qualquer tecla para continuar...");
+			Console.ReadKey(true);
+		}
+	}
+}
